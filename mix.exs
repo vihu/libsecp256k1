17e6229b4168e8e646f5dacc36017e9f550323eb
Antoine Toulme <antoine@lunar-ocean.com>
2018-08-20 10:56:28 -0700

    Fix the files option in mix.exs

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Mix.Tasks.Compile.MakeBindings do
-
   def run(_) do
     {_, exit_code} = System.cmd("make", [], into: IO.stream(:stdio, :line))
+
     case exit_code do
       0 -> :ok
       _ -> :error
@@ -13,26 +13,36 @@ defmodule Libsecp256k1.Mixfile do
   use Mix.Project
 
   def project do
-    {filelist, 0} = System.cmd("git", ["ls-files"])
-    [app: :libsecp256k1,
-     version: "0.1.8",
-     language: :erlang,
-     description: "Erlang NIF bindings for the the libsecp256k1 library",
-     package: [
-       files: Enum.drop(String.split(filelist, "\n"), -1),
-       maintainers: ["Matthew Branton", "Geoffrey Hayes"],
-       licenses: ["MIT"],
-       links: %{"GitHub" => "https://github.com/exthereum/libsecp256k1"}
-     ],
-     compilers: [:make_bindings, :erlang, :app],
-     deps: deps()
-     ]
+    [
+      app: :libsecp256k1,
+      version: "0.1.8",
+      language: :erlang,
+      description: "Erlang NIF bindings for the the libsecp256k1 library",
+      package: [
+        files: [
+          "LICENSE",
+          "Makefile",
+          "README.md",
+          "c_src/build_deps.sh",
+          "c_src/libsecp256k1_nif.c",
+          "etest/libsecp256k1_tests.erl",
+          "mix.exs",
+          "priv/.empty",
+          "src/libsecp256k1.erl"
+        ],
+        maintainers: ["Matthew Branton", "Geoffrey Hayes"],
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/exthereum/libsecp256k1"}
+      ],
+      compilers: [:make_bindings, :erlang, :app],
+      deps: deps()
+    ]
   end
 
   defp deps() do
     [
       {:mix_erlang_tasks, "0.1.0", runtime: false},
-      {:ex_doc, "~> 0.17", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.17", only: :dev, runtime: false}
     ]
   end
 end
